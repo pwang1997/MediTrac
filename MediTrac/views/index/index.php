66f34a3ad248fd7d3ca87f26ea2f7d308ee5f370
Courtney Gosselin <courtney@gosselin.io>
2020-01-18 15:45:51 -0800

    Testing server with index.php
    Signed-off-by: Courtney Gosselin <courtney@gosselin.io>

---
+++ MediTrac/views/index/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+    <html>        
+        <?php include '../../shared/head.html';?>
+        <body>
+        <?php include '../../shared/header.html';?>
+            <main>
+                <p>Hello World!</p>
+            </main>
+            <?php include '../../shared/footer.html';?>
+        </body>
+    </html>
