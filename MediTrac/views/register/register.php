2ba1d8c611c15f13084bb5f4b463fc5000c1cdb1
pwang1997 <puck19970418@gmail.com>
2020-01-18 20:12:35 -0800

    added register page and validation

--- MediTrac/views/register/register.php
+++ MediTrac/views/register/register.php
@@ -1,46 +1,48 @@
 <!DOCTYPE html>
 <html>
-<?php include '../shared/head.html'; ?>
-
-<body>
-    <?php include '../shared/header.html'; ?>
+    <?php include '../shared/head.html'; ?>
+    <script src="register.js"></script>
+    
+    <body>
+        <?php include '../shared/header.html'; ?>
     <main>
         <div class="mx-auto" style="width: 600px;">
             <form id="registerForm" >
-                <h4 class="mx-auto">Register</h4>
+                <div class="row">
+                    <h4 class="offset-md-6">Register</h4>
+                </div>
 
                 <div class="form-group row">
-                    <label for="inputPassword" class="col-sm-2 col-form-label">Username</label>
+                    <label for="inputPassword" class="col-sm-2 col-form-label form-control-label">Username</label>
                     <div class="col-sm-10">
-                        <input type="text" class="form-control" id="username" name="username">
+                        <input type="text" class="form-control" id="username" name="username" required>
                     </div>
                 </div>
                 <div class="form-group row">
-                    <label for="inputPassword" class="col-sm-2 col-form-label">Email</label>
+                    <label for="inputPassword" class="col-sm-2 col-form-label form-control-label">Email</label>
                     <div class="col-sm-10">
-                        <input type="email" class="form-control" id="email" name="email">
+                        <input type="email" class="form-control" id="email" name="email" required>
                     </div>
                 </div>
                 <div class="form-group row">
-                    <label for="inputPassword" class="col-sm-2 col-form-label">Password</label>
+                    <label for="inputPassword" class="col-sm-2 col-form-label form-control-label">Password</label>
                     <div class="col-sm-10">
-                        <input type="password" class="form-control" id="password" name="password">
+                        <input type="password" class="form-control" id="password" name="password" required>
                     </div>
                 </div>
                 <div class="form-group row">
-                    <label for="inputPassword" class="col-sm-2 col-form-label">Confirm Password</label>
+                    <label for="inputPassword" class="col-sm-2 col-form-label form-control-label">Confirm Password</label>
                     <div class="col-sm-10">
-                        <input type="password" class="form-control" id="password2" name="password2">
+                        <input type="password" class="form-control" id="password2" name="password2" required>
                     </div>
                 </div>
                 <div class="row">
-                    <button type="submit" class="btn btn-primary">Submit</button>
-                    <button type="reset" class="btn btn-primary">Reset</button>
+                    <button type="reset" class="btn btn-primary col-md-4">Reset</button>
+                    <button type="submit" class="btn btn-primary col-md-4 offset-md-4" id="submit">Submit</button>
                 </div>
         </div>
         </form>
     </main>
     <?php include '../shared/footer.html'; ?>
 </body>
-
 </html>
