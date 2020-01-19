16909bf6c112e13afc03b6966218ef70e407d4d2
Ben Tisserand <43098587+ubcbent@users.noreply.github.com>
2020-01-18 22:28:16 -0800

    i hate git why cant I push im stuck in file upload hell

---
+++ MediTrac/views/admin_tools/db_queries.php
@@ -0,0 +1,24 @@
+<?php
+
+include_once 'db_connector.php';
+
+$mysqli = new mysqli("34.94.246.220", "root", "qwerty1", "peaceful-berm-265521:us-west2:meditrac");
+$sql = "SELECT userName FROM user";
+if (!$result = $mysqli->query($sql)) {
+    // Oh no! The query failed. 
+    echo "Sorry, the website is experiencing problems.";
+
+    // Again, do not do this on a public site, but we'll show you how
+    // to get the error information
+    echo "Error: Our query failed to execute and here is why: \n";
+    echo "Query: " . $sql . "\n";
+    echo "Errno: " . $mysqli->errno . "\n";
+    echo "Error: " . $mysqli->error . "\n";
+    exit;
+}
+$result->fetch_assoc();
+
+echo($result['email']);
+
+
+?>
