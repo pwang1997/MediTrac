1de047fc12c72d6b07c818aa50c4fdaa1bc2368a
ubcbent <bentisserand98@gmail.com>
2020-01-19 03:33:23 -0800

    testing query
    Signed-off-by: ubcbent <bentisserand98@gmail.com>

--- MediTrac/views/admin_tools/db_queries.php
+++ MediTrac/views/admin_tools/db_queries.php
@@ -16,12 +16,21 @@ catch(PDOException $e)
     foreach(getSymptomsFromUser($con,2) as $row){
         echo "<p>" .$row['name'] . "</p>";
     }
+    foreach(getSymptomEventsForMonth($con,1,1,2020) as $row){
+        echo "<p>" .$row['date'] . "</p>";
+    }
     function getUserFromLogin($con, $email, $password){
         $sql = "SELECT * FROM user WHERE email = '$email' AND password = '$password'";
         return executeSql($con,$sql)->fetch();
     }
     function getSymptomsFromUser($con,$userId){
         $sql = "SELECT * FROM symptom WHERE userId = $userId";
+        return executeSql($con,$sql)->fetchAll();
+    }
+    function getSymptomEventsForMonth($con,$symptomId,$month,$year){
+        $endMonth = ($month == 12 ? 1 : $month+1);
+        $endYear = ($month == 12 ? $year+1 : $year);
+        $sql = "SELECT * FROM symptomEvents WHERE symptomId = $symptomId AND date >= '$year-$month-0 00:00:00' AND date < '$endYear-$endMonth-0 00:00:00'";
         return executeSql($con,$sql)->fetchAll();
     }
 
