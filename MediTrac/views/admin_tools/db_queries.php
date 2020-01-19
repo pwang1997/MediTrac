9a007bf3fdbfd6a74d531d8f874f251eac3b633b
ubcbent <bentisserand98@gmail.com>
2020-01-19 02:04:37 -0800

    testing query
    Signed-off-by: ubcbent <bentisserand98@gmail.com>

--- MediTrac/views/admin_tools/db_queries.php
+++ MediTrac/views/admin_tools/db_queries.php
@@ -1,9 +1,9 @@
 <?php
 
 try {
-    $conn = new PDO("mysql:host=34.94.246.220;dbname=meditrac", "root", "qwerty1");
+    $con = new PDO("mysql:host=34.94.246.220;dbname=meditrac", "root", "qwerty1");
     // set the PDO error mode to exception
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     
     }
 catch(PDOException $e)
@@ -12,11 +12,11 @@ catch(PDOException $e)
     die();
     }
 
-    echo getUserFromLogin('jane@doe.ca','passy')['id'];
+    echo getUserFromLogin($con,'jane@doe.ca','passy')['id'];
 
-    function getUserFromLogin($email, $password){
+    function getUserFromLogin($con, $email, $password){
         $sql = "SELECT * FROM user WHERE \"email\" = $email AND \"password\" = $password";
-        $stmt = $conn->prepare($sql);
+        $stmt = $con->prepare($sql);
         $stmt->execute();
         $stmt->setFetchMode(PDO::FETCH_ASSOC);
         return $stmt->fetch(); // assumed to be a single return value
