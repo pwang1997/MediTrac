1441ab21ce5b2683080a7d91995052f8c5cbc046
pwang1997 <puck19970418@gmail.com>
2020-01-18 23:05:36 -0800

    updated btn save in index
    Signed-off-by: pwang1997 <puck19970418@gmail.com>

--- index.php
+++ index.php
@@ -47,7 +47,7 @@
                             </div>
                             <div class="modal-footer">
                                 <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                                <button type="button" class="btn btn-primary">Save</button>
+                                <button type="button" class="btn btn-primary" id="btnSave">Save</button>
                             </div>
                         </div>
                     </div>
@@ -70,5 +70,7 @@
             '<div class="col-sm-10"><input type="text" class="form-control" id="newSympotom" name="newSympotom"></div></div>');
             }
         });
+
+
     });
 </script>
