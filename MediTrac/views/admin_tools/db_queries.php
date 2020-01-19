c8a192065a04cb493693b8cdb3a95af88da606a2
ubcbent <bentisserand98@gmail.com>
2020-01-19 01:46:51 -0800

    testing query
    Signed-off-by: ubcbent <bentisserand98@gmail.com>

--- MediTrac/views/admin_tools/db_queries.php
+++ MediTrac/views/admin_tools/db_queries.php
@@ -4,28 +4,23 @@ try {
     $conn = new PDO("mysql:host=34.94.246.220;dbname=meditrac", "root", "qwerty1");
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully";
+    
     }
 catch(PDOException $e)
     {
     echo "Connection failed: " . $e->getMessage();
+    die()
     }
-/*$sql = "SELECT userName FROM user";
-if (!$result = $mysqli->query($sql)) {
-    // Oh no! The query failed. 
-    echo "Sorry, the website is experiencing problems.";
+$sql = "SELECT userName FROM user";
+$stmt = $conn->prepare($sql);
+    $stmt->execute();
 
-    // Again, do not do this on a public site, but we'll show you how
-    // to get the error information
-    echo "Error: Our query failed to execute and here is why: \n";
-    echo "Query: " . $sql . "\n";
-    echo "Errno: " . $mysqli->errno . "\n";
-    echo "Error: " . $mysqli->error . "\n";
-    exit;
-}
-$result->fetch_assoc();
+    // set the resulting array to associative
+    $results = $stmt->setFetchMode(PDO::FETCH_ASSOC);
 
-echo($result['email']);*/
+    while($result = $results->fetch()){
+        echo $result['userName'];
+    }
 
-
+    
 ?>
