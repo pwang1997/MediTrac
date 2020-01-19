1577bc736249a5987381afa238ec2515d0321b81
ubcbent <bentisserand98@gmail.com>
2020-01-19 02:06:59 -0800

    testing query
    Signed-off-by: ubcbent <bentisserand98@gmail.com>

--- MediTrac/views/admin_tools/db_queries.php
+++ MediTrac/views/admin_tools/db_queries.php
@@ -16,6 +16,7 @@ catch(PDOException $e)
 
     function getUserFromLogin($con, $email, $password){
         $sql = "SELECT * FROM user WHERE \"email\" = '$email' AND \"password\" = '$password'";
+        echo $sql;
         $stmt = $con->prepare($sql);
         $stmt->execute();
         $stmt->setFetchMode(PDO::FETCH_ASSOC);
