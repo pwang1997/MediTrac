f96506c2318c6ce091980f446bbb8c0f192a9e32
ubcbent <bentisserand98@gmail.com>
2020-01-19 05:47:48 -0800

    masdf
    Signed-off-by: ubcbent <bentisserand98@gmail.com>

--- MediTrac/views/admin_tools/loginProcess.php
+++ MediTrac/views/admin_tools/loginProcess.php
@@ -6,7 +6,7 @@
     $email = $_POST["email"];
     $password = $_POST['password'];
 
-   $result = (getUserFromLogin($con, $email,($password)));
+   $result = (getUserFromLogin(connect(), $email,($password)));
 
    $_SESSION['user'] = $result['id'];
 
