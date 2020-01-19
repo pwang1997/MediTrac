fc31e2aa9e227a1dde228038ded6d3effe536313
ubcbent <bentisserand98@gmail.com>
2020-01-19 06:26:46 -0800

    masdf
    Signed-off-by: ubcbent <bentisserand98@gmail.com>

--- MediTrac/views/admin_tools/loginProcess.php
+++ MediTrac/views/admin_tools/loginProcess.php
@@ -8,6 +8,5 @@
 
    $result = (getUserFromLogin(connect(), $email,($password)));
    $_SESSION['user'] = $result['id'];
-    echo isset($_SESSION['user']) ? $_SESSION['user'] : "SESSION USER NOT SET";
-   //header("Location:"."../../../index.php");
+   header("Location:"."../../../index.php");
 ?>
