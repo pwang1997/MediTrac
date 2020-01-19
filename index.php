7da8439fbf55ad1649c5912f4270b6663c0c0d2a
pwang1997 <puck19970418@gmail.com>
2020-01-18 22:09:54 -0800

    added draggable event
    Signed-off-by: pwang1997 <puck19970418@gmail.com>

--- index.php
+++ index.php
@@ -1,21 +1,31 @@
 <!DOCTYPE html>
-    <html>        
-        <?php include './MediTrac/views/shared/head.html';?>
-        <body>
-        <?php include './MediTrac/views/shared/header.html';?>
-            <main>
-                <div class="container">
-                    <div class="row">
-                        <div class="col-4">
-                            <input type="text" placeholder="Search..">
-                            <div id='draggable-el' data-event='{ "title": "my event", "duration": "02:00" }'>drag me!! Test!</div>
-                        </div>
-                        <div class="col-8">
-                            <div id='calendar'></div>
+<html>
+<?php include './MediTrac/views/shared/head.html'; ?>
+
+<body>
+    <?php include './MediTrac/views/shared/header.html'; ?>
+    <main>
+        <div class="container">
+            <div class="row">
+                <div class="col-4">
+                    <input type="text" placeholder="Search..">
+                    <div id='external-events'>
+                        <h4>Draggable Events</h4>
+                        <div id='draggable-el'>
+                            <div class="event">My Event 1</div>
+                            <div class='event'>My Event 2</div>
                         </div>
                     </div>
+
+                    <!-- <div id='draggable-el' data-event='{ "title": "my event", "duration": "02:00" }'>drag me!! Test!</div> -->
                 </div>
-            </main>
-            <?php include './MediTrac/views/shared/footer.html';?>
-        </body>
-    </html>
+                <div class="col-8">
+                    <div id='calendar'></div>
+                </div>
+            </div>
+        </div>
+    </main>
+    <?php include './MediTrac/views/shared/footer.html'; ?>
+</body>
+
+</html>
