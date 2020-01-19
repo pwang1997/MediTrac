25737de76f029e6cad563529cedd95f693982e53
ubcbent <bentisserand98@gmail.com>
2020-01-18 23:30:20 -0800

    isolating error
    Signed-off-by: ubcbent <bentisserand98@gmail.com>

--- MediTrac/views/admin_tools/db_queries.php
+++ MediTrac/views/admin_tools/db_queries.php
@@ -1,8 +1,8 @@
 <?php
 
-try{
+
 $mysqli = new mysqli("34.94.246.220", "root", "qwerty1", "peaceful-berm-265521:us-west2:meditrac");
-$sql = "SELECT userName FROM user";
+/*$sql = "SELECT userName FROM user";
 if (!$result = $mysqli->query($sql)) {
     // Oh no! The query failed. 
     echo "Sorry, the website is experiencing problems.";
@@ -17,10 +17,7 @@ if (!$result = $mysqli->query($sql)) {
 }
 $result->fetch_assoc();
 
-echo($result['email']);
-}catch(Exception $e){
-    echo $e->getMessage;
-}
+echo($result['email']);*/
 
 
 ?>
