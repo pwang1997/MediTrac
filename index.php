6cf23e3927d20935924c27f88433adb6a039a1dd
Courtney Gosselin <courtney@gosselin.io>
2020-01-18 16:07:45 -0800

    Testing php
    Signed-off-by: Courtney Gosselin <courtney@gosselin.io>

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
     <html>        
-        <?php include '../../shared/head.html';?>
+        <?php include './MediTrac/views/shared/head.html';?>
         <body>
-        <?php include '../../shared/header.html';?>
+        <?php include './MediTrac/views/shared/header.html';?>
             <main>
                 <p>Hello World!</p>
             </main>
-            <?php include '../../shared/footer.html';?>
+            <?php include './MediTrac/views/shared/footer.html';?>
         </body>
     </html>
