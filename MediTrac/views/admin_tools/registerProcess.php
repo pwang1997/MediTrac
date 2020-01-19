9ef0e43877b15f73f2cd099526a87678408133e5
ubcbent <bentisserand98@gmail.com>
2020-01-19 09:12:42 -0800

    add registration
    Signed-off-by: ubcbent <bentisserand98@gmail.com>

--- MediTrac/views/admin_tools/registerProcess.php
+++ MediTrac/views/admin_tools/registerProcess.php
@@ -4,9 +4,13 @@
 
     if(!isset($_SESSION)) { session_start();}
     $email = $_POST["email"];
+    $username = $_POST["username"];
     $password = $_POST['password'];
 
-   $result = (getUserFromLogin(connect(), $email,($password)));
+    $con = connect();
+   insertUser($con, $username,$email,$password);
+   $result = getUserFromLogin($con,$email,$password);
    $_SESSION['user'] = $result['id'];
+   $con = null;
    header("Location:"."../../../index.php");
 ?>
