c2366c1d4d22b09d630449b31947f7868cf7e988
Courtney Gosselin <courtney@gosselin.io>
2020-01-19 07:53:10 -0800

    add button side by side
    Signed-off-by: Courtney Gosselin <courtney@gosselin.io>

--- index.php
+++ index.php
@@ -90,7 +90,7 @@ if(!isset($_SESSION['user'])){
         <div class="container">
             <div class="row">
                 <div class="col-4 container p-2 border bg-dark text-light">
-                    <form action="" method="POST" id="addSymptom" class="d-inline w-100">
+                    <form action="" method="POST" id="addSymptom" class="d-block">
                         <input type="text" placeholder="Add new" class="p-2 mr-2 mt-2 mb-2"/>
                         <button type="submit" form="addSymptom" value="Submit"class="p-2">add</button>
                     </form>
