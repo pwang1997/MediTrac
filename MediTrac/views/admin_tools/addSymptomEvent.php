c59130cc2bf1d7cac634b21020593140796b75c5
Greg Petsul <45180610+GregPetsul@users.noreply.github.com>
2020-01-19 07:44:31 -0800

    working on ajax
    Signed-off-by: Greg Petsul <45180610+GregPetsul@users.noreply.github.com>

--- MediTrac/views/admin_tools/addSymptomEvent.php
+++ MediTrac/views/admin_tools/addSymptomEvent.php
@@ -4,11 +4,14 @@
     $start = $_POST['start'];
     $end = $_POST['end'];
 
+    if(!isset($_SESSION)) { session_start();}
+    echo $_SESSION['user'];
+    print_r(getSymptomsFromUser(connect(), $_SESSION['user']));
     if(isset($_SESSION['user'])) {
-        $symptoms = getSymptomsFromUser($connection, $_SESSION['user']);
+        $symptoms = getSymptomsFromUser(connect(), $_SESSION['user']);
         foreach($symptoms as $symptom) {
             if($symptom['name'] === $title) {
-                echo insertSymptomEvent($connection, $symptom['id'], $start);
+                echo insertSymptomEvent(connect(), $symptom['id'], $start);
             break;
             }
         }
