7e9bc1544ebcba28223fa6c3551c937d7e7e4f32
pwang1997 <puck19970418@gmail.com>
2020-01-18 23:45:09 -0800

    abcd
    Signed-off-by: pwang1997 <puck19970418@gmail.com>

--- index.php
+++ index.php
@@ -36,11 +36,11 @@
                             <div class="modal-body">
                                 <form>
                                     <div class="form-group">
-                                        <label for="role">Sympotom</label>
-                                        <select class="form-control" id="sympotomList" name="sympotomList">
+                                        <label for="role">symptom</label>
+                                        <select class="form-control" id="symptomList" name="symptomList">
                                             <option>Headache</option>
                                             <option>Fever</option>
-                                            <option id="addNewSympotom">Add New Sympotom</option>
+                                            <option id="addNewsymptom">Add New symptom</option>
                                         </select>
                                     </div>
                                 </form>
@@ -63,11 +63,11 @@
 
 <script>
     $(document).ready(() => {
-        $("#sympotomList").change(function() {
-            var selectedSympotom = $(this).children("option:selected").val();
-            if (selectedSympotom === "Add New Sympotom") {
-                $(this).parent().append('<div class="form-group row"><label for="newSympotom" class="col-sm-2 col-form-label form-control-label">Sympotom</label>' +
-            '<div class="col-sm-10"><input type="text" class="form-control" id="newSympotom" name="newSympotom"></div></div>');
+        $("#symptomList").change(function() {
+            var selectedsymptom = $(this).children("option:selected").val();
+            if (selectedsymptom === "Add New symptom") {
+                $(this).parent().append('<div class="form-group row"><label for="newsymptom" class="col-sm-2 col-form-label form-control-label">symptom</label>' +
+            '<div class="col-sm-10"><input type="text" class="form-control" id="newsymptom" name="newsymptom"></div></div>');
             }
         });
 
