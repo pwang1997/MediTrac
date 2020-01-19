150405421b48aae1921f49ca242153636ef9847f
Courtney Gosselin <courtney@gosselin.io>
2020-01-19 00:00:50 -0800

    PDO testing
    Signed-off-by: Courtney Gosselin <courtney@gosselin.io>

--- MediTrac/views/admin_tools/db_queries.php
+++ MediTrac/views/admin_tools/db_queries.php
@@ -24,3 +24,19 @@ echo($result['email']);
 
 
 ?>
+<?php
+/* Attempt MySQL server connection. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+try{
+    $pdo = new PDO("mysql:host=34.94.246.220"", "root", "qwerty1");
+    
+    // Set the PDO error mode to exception
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+    // Print host information
+    echo "Connect Successfully. Host info: " . 
+$pdo->getAttribute(constant("PDO::ATTR_CONNECTION_STATUS"));
+} catch(PDOException $e){
+    die("ERROR: Could not connect. " . $e->getMessage());
+}
+?>
